57a637e417601b663da4f6c274806d1cd659e8a6
Fuad303 <sadiqovfuad8@gmail.com>
2023-03-04 02:17:32 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import DataItem from './components/DataItem'
+import Basket from './components/Basket'
+import { RxHamburgerMenu } from 'react-icons/rx'
 
-function App() {
+export default function App() {
+
+  const [array, setArray] = useState([])
+  const [basket, setBasket] = useState([])
+  const [price, setPrice] = useState(6000)
+  const [open, setOpen] = useState(false)
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+    .then(res => res.json())
+    .then(data => setArray(Object.entries(data)))
+    .catch(error => {
+      console.log(error)
+    })
+  }, [])
+  const toggleOpen = () => {
+    setOpen(!open)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='bg-black'>
+      <header className='header text-black w-full py-3 flex justify-between px-6 text-lg fixed bg-white'>
+          <b>Total money: ${price}</b>
+          <RxHamburgerMenu className='pt-1 text-2xl cursor-pointer' onClick={toggleOpen} />
       </header>
+      <Basket setBasket={setBasket} setOpen={setOpen} open={open} basket={basket}/>
+      <ul className='pt-20 grid  grid-cols-3'>
+        {array.map((item, index) => {
+          return <DataItem basket={basket} setBasket={setBasket} price={price} key={index} index={index} item={item} setPrice={setPrice}/>
+        })}      
+      </ul>
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/components/DataItem.jsx
@@ -0,0 +1,63 @@
+export default function DataItem({item, price, setPrice, setBasket, basket, index}) {
+
+  const addCart = (item) => {
+    const checkBasket = basket.find(basketItem => basketItem.id === item[1].id)
+    if(checkBasket){
+      checkBasket.amount += 1;
+      setBasket([...basket.filter(basketItem => basketItem.id !== item[1].id), checkBasket])
+    }
+    else{
+      setBasket([
+        ...basket, 
+          {
+            id: item[1].id,
+            title: item[1].title,
+            image: item[1].image,
+            price: item[1].price,
+            amount: 1
+          }
+      ])
+    }
+    setPrice(price - item[1].price)
+  }
+    const removeCart = (item) => { 
+    const currentBasket = basket.find(basketItem => basketItem.id === item[1].id)
+    const basketWithoutCurrent = basket.filter(basketItem => basketItem.id !== item[1].id)
+    currentBasket.amount -= 1;
+    if(currentBasket.amount === 0){
+      setBasket([...basketWithoutCurrent])
+    }
+    else{
+      setBasket([...basketWithoutCurrent, currentBasket])
+    }
+    setPrice(price + item[1].price)
+
+  }
+  return (
+    <li key={index} className='h-96 my-2'>
+    <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <div className='flex justify-center mb-2'>
+          <img className="w-full object-cover h-40 rounded-t-lg"  src={item[1].image} alt="" />
+        </div>
+        <div className="px-5 pb-5">
+            <a href="#">
+                <h5 className="text-lg h-16 font-semibold tracking-tight text-gray-900 dark:text-white">{item[1].title}</h5>
+            </a>
+            <div className="flex items-center mt-2.5 mb-5">
+                <svg aria-hidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                <svg aria-hidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Second star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                <svg aria-hidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Third star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                <svg aria-hidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fourth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                <svg aria-hidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fifth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">5.0</span>
+            </div>
+            <p className="text-xl font-bold text-gray-900 dark:text-white">${item[1].price}</p>
+            <div className="flex items-center justify-between">
+                <button disabled={price <= item[1].price} type='submit' onClick={() => addCart(item)} className="flex-1 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</button>
+                <button disabled={!basket.find((b, id) => b.id === item[1].id)} type='submit' onClick={() => removeCart(item)} className="flex-1 text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Remove from cart</button>
+            </div>
+        </div>
+    </div>
+    </li>
+  )
+}
